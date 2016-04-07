21dbd4072af3ff8fa132fb9dd8adfd8c7657405f
maria <dalfonso@cern.ch>
2016-04-07 15:48:23 +0200

    set the GT and jer as Fall15_25nsV2 (this is the legacy of the 76X)

--- PhysicsTools/PatAlgos/test/corMETFromMiniAOD.py
+++ PhysicsTools/PatAlgos/test/corMETFromMiniAOD.py
@@ -40,15 +40,15 @@ redoPuppi=False # rebuild puppiMET
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 #from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
 from Configuration.AlCa.autoCond import autoCond
-#GlobalTag = gtCustomise(GlobalTag, 'auto:run2_data', '')
+##___________________________External JECs________________________________||
 if runOnData:
-  process.GlobalTag.globaltag = autoCond['run2_data']
-  #process.GlobalTag.globaltag = '75X_dataRun1_v2' #'74X_dataRun2_Prompt_v1'
+ process.GlobalTag.globaltag = '76X_dataRun2_16Dec2015_v0'
 else:
-  process.GlobalTag.globaltag = 'MCRUN2_75_V5'
- # process.GlobalTag.globaltag = autoCond['run2_mc']
- # process.GlobalTag = GlobaTag(GlobalTag, 'auto:run2_mc', '')
+ process.GlobalTag.globaltag = '76X_mcRun2_asymptotic_RunIIFall15DR76_v1'
 
+## Private SQlite files to be taken from
+## https://twiki.cern.ch/twiki/bin/view/CMS/JECDataMC#Recommended_for_Data
+## https://twiki.cern.ch/twiki/bin/view/CMS/JECDataMC#Recommended_for_MC
 if usePrivateSQlite:
     from CondCore.DBCommon.CondDBSetup_cfi import *
     import os
@@ -77,27 +77,30 @@ if usePrivateSQlite:
 ### =====================================================================================================
 #process.load("JetMETCorrections.Modules.JetResolutionESProducer_cfi")
 from CondCore.DBCommon.CondDBSetup_cfi import *
+
+##___________________________External JER file________________________________||
+##https://github.com/cms-jet/JRDatabase/tree/master/SQLiteFiles
 process.jer = cms.ESSource("PoolDBESSource",CondDBSetup,
                            #connect = cms.string( "frontier://FrontierPrep/CMS_COND_PHYSICSTOOLS"),
                            #connect = cms.string( "frontier://FrontierPrep/CMS_CONDITIONS"),
-                           connect = cms.string("sqlite:PhysicsTools/PatUtils/data/Summer15_25nsV6_MC.db"),
+                           connect = cms.string("sqlite:PhysicsTools/PatUtils/data/Fall15_25nsV2_MC.db"),
                            toGet =  cms.VPSet(
     cms.PSet(
       record = cms.string('JetResolutionRcd'),
       #tag    = cms.string('JR_MC_PtResolution_Summer15_25nsV6_AK4PF'),
-      tag    = cms.string('JR_Summer15_25nsV6_MC_PtResolution_AK4PFchs'),
+      tag    = cms.string('JR_Fall15_25nsV2_MC_PtResolution_AK4PFchs'),
       label  = cms.untracked.string('AK4PFchs_pt')
       ),
     cms.PSet(
       record = cms.string("JetResolutionRcd"),
       #tag = cms.string("JR_MC_PhiResolution_Summer15_25nsV6_AK4PF"),
-      tag = cms.string("JR_Summer15_25nsV6_MC_PhiResolution_AK4PFchs"),
+      tag = cms.string("JR_Fall15_25nsV2_MC_PhiResolution_AK4PFchs"),
       label= cms.untracked.string("AK4PFchs_phi")
       ),
     cms.PSet(
       record = cms.string('JetResolutionScaleFactorRcd'),
       #tag    = cms.string('JR_DATAMCSF_Summer15_25nsV6_AK4PFchs'),
-      tag    = cms.string('JR_Summer15_25nsV6_MC_SF_AK4PFchs'),
+      tag    = cms.string('JR_Fall15_25nsV2_MC_SF_AK4PFchs'),
       label  = cms.untracked.string('AK4PFchs')
       ),
     
@@ -109,7 +112,7 @@ process.es_prefer_jer = cms.ESPrefer("PoolDBESSource",'jer')
 if runOnData:
   #75X file : root://eoscms//eos/cms/store/relval/CMSSW_7_5_0/SingleElectron/MINIAOD/75X_dataRun1_HLT_v1_RelVal_electron2012D-v1/00000/A4BD1262-8F2B-E511-8470-002618943964.root
   #74X file : root://eoscms.cern.ch//store/data/Run2015B/JetHT/MINIAOD/PromptReco-v1/000/251/252/00000/263D331F-AF27-E511-969B-02163E012627.root
-  fname = 'root://eoscms.cern.ch//store/data/Run2015B/JetHT/MINIAOD/PromptReco-v1/000/251/252/00000/263D331F-AF27-E511-969B-02163E012627.root' 
+  fname = 'root://eoscms.cern.ch//store/data/Run2015D/SingleElectron/MINIAOD/PromptReco-v4/000/260/607/00000/36558DE4-1484-E511-AA5C-02163E01442E.root'
 else:
   #75X file : root://eoscms.cern.ch//store/relval/CMSSW_7_5_0/RelValTTbar_13/MINIAODSIM/75X_mcRun2_asymptotic_v1-v1/00000/92A928E7-842A-E511-87CC-0025905A60E0.root
   #74X file : root://eoscms.cern.ch//store/mc/RunIISpring15DR74/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/Asympt50ns_MCRUN2_74_V9A-v2/60000/001C7571-0511-E511-9B8E-549F35AE4FAF.root
